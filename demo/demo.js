18783c77de81169fe29d54ace4ef50c099506b5d
caojiang <likecao@gmail.com>
2018-01-17 22:56:46 +0800

    fix demo.js

--- demo/demo.js
+++ demo/demo.js
@@ -5,7 +5,7 @@ const map = new maptalks.Map('map', {
         content: '&copy; <a href="http://osm.org">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/">CARTO</a>'
     },
     baseLayer: new maptalks.TileLayer('base', {
-        urlTemplate: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+        urlTemplate: 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
         subdomains: ['a','b','c','d']
     })
 })
